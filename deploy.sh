4568af9c6995c0deae1a01c2cbfeede7b4eccac5
Knut Hühne <knut@k-nut.eu>
2017-10-16 20:57:26 +0200

    add service worker to store website offline

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 scp index.html knut@k-nut.eu:/var/www/virtual/knut/html
+scp sw.js knut@k-nut.eu:/var/www/virtual/knut/html
---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+ e.waitUntil(
+   caches.open('k-nut').then((cache) => {
+     return cache.addAll([
+       '/',
+       '/index.html',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
